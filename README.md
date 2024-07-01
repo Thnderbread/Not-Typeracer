3b07feee1227bc77dd7d43220bc1948e13d34b50
Bobby <109012458+Thnderbread@users.noreply.github.com>
2024-07-01 05:11:44 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# typeracer_clone
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run server.ts
+```
---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import typescriptParser from "@typescript-eslint/parser"
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import html from "@html-eslint/eslint-plugin"
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "comma-dangle": "off",
+      "@typescript-eslint/comma-dangle": "off",
+      "space-before-function-paren": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: false },
+      ],
+      "@typescript-eslint/no-base-to-string": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      quotes: [
+        "error",
+        "double",
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true,
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          trailingComma: "es5",
+          tabWidth: 2,
+          printWidth: 80,
+          arrowParens: "always",
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+  {
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+      globals: globals.node,
+    },
+  },
+  {
+    ignores: ["*.html"],
+  },
+  {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+  },
+]
